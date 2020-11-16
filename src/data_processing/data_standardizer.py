0457ac22b8ed21501c4ceaff8b0596cdeae69635
Wojciech <w.wegrzynek@gmail.com>
2020-11-16 14:01:27 +0100

    data_standardizer + gaussian basis functions

---
+++ src/data_processing/data_standardizer.py
@@ -0,0 +1,24 @@
+class DataStandardizer():
+    def normalize(self, dataset): #not standardize to be able to substitute DataNormalizer with this class (could be done more elegantly with common ancestor, but this also works)
+        data_lines=dataset.data_lines()
+        mins=data_lines[0]
+        maxes=data_lines[0]
+        for line in data_lines:
+            for i in range(len(line)):
+                if mins[i]>line[i]:
+                    mins[i]=line[i]
+                if maxes[i]<line[i]:
+                    maxes[i]=line[i]
+        def standardizer_func(min, max):
+            def standardize(x):
+                return (x-min)/(max-min)
+            return standardize
+        standardizer_funcs=[standardizer_func(mins[i],maxes[i]) for i in range(len(maxes))]
+        standardized_dataset=dataset.apply_functions(standardizer_funcs)
+
+        #technically already standardized, but it's gonna be easier if mean=0
+        mean=standardized_dataset.columnwise_mean()
+        def norm(mean):
+            return lambda num: (num-mean)
+        functions=[norm(mean[i]) for i in range(dataset.y)]
+        return standardized_dataset.apply_functions(functions)
--- src/model_calculations/validators/gaussian_regression_validator.py
+++ src/model_calculations/validators/gaussian_regression_validator.py
@@ -1,11 +1,11 @@
 from .hyperparameter_regression_validator import HyperparameterRegressionValidator
-
-class PolynomialRegressionValidator(HyperparameterRegressionValidator):
+from math import exp
+class GaussianRegressionValidator(HyperparameterRegressionValidator):
 
     def __init__(self, training_dataset, validation_dataset,degrees=[1,2,3,4,5],lambda_val=[0,0.1,0.4,1.6]):
         super().__init__(training_dataset,validation_dataset,degrees,lambda_val=lambda_val)
 
     def get_basis_func(self, params):
-        def get_power(degree):
-            return lambda x: x**degree
-        return [get_power(degree) for degree in params]
+        def gauss(s):
+            return lambda x: exp(x^2/s^2) 
+        return [gauss(s) for s in params]
